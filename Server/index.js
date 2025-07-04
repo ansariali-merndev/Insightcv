56c19963e1a7189a259bbed6360a3136475ffaf0
ansariali <=>
2025-07-04 20:01:27 +0530

    fix cors

--- Server/index.js
+++ Server/index.js
@@ -10,12 +10,7 @@ configDotenv();
 const app = express();
 const port = process.env.PORT;
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", rootRouter);
